f00369e0b6ec8dfaedb60e09cb702efb4a4df279
lirui <3191113206@qq.com>
2025-10-29 16:37:50 +0800

    day04 新增菜品接口

---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,39 @@
+package com.sky.service.impl;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class DishServiceImpl implements DishService {
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
+    /**
+     * 新增菜品
+     * @param dishDto
+     */
+    @Transactional
+    public void saveWithFlavor(DishDTO dishDto) {
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDto, dish);
+        // 新增菜品
+        dishMapper.insert(dish);
+        Long dishId = dish.getId();
+        List<DishFlavor> flavors = dishDto.getFlavors();
+        flavors.forEach(flavor -> {
+            flavor.setDishId(dishId);
+        });
+        dishFlavorMapper.insertBatch(flavors);
+    }
+}
